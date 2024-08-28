233f72446e3b8a988929ba29edc0f3305b3ccab9
Roger Alencar <rogersalencar@hotmail.com>
2024-08-28 08:37:54 -0300

    context para consumo de API e hooks

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,9 +1,15 @@
+import { useContext } from "react";
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
 import { SearchForm } from "./components/SearchForm";
 import { PriceHighlight, TransactionsContainer, TransactionTable } from "./styles";
+import { TransactionsContext } from "../../contexts/TransactionContext";
+import { dateFormatter, priceFormatter } from "../../utils/formatter";
+
 
 export function Transactions() {
+    const { transactions } = useContext(TransactionsContext);
+
     return (
         <div>
             <Header />
@@ -13,26 +19,21 @@ export function Transactions() {
 
                 <TransactionTable>
                     <tbody>
-                        <tr>
-                            <td width="40%">Desenvolvimento de site</td>
-                            <td>
-                                <PriceHighlight variant="income">
-                                    R$ 12.000,00
-                                </PriceHighlight>
-                            </td>
-                            <td>Venda</td>
-                            <td>13/04/2022</td>
-                        </tr>
-                        <tr>
-                            <td width="40%">Hambúrguer</td>
-                            <td>
-                                <PriceHighlight variant="outcome">
-                                    - R$ 12.000,00
-                                </PriceHighlight>
-                                </td>
-                            <td>Alimentação</td>
-                            <td>13/04/2022</td>
-                        </tr>
+                        {transactions.map(transaction => {
+                            return (
+                                <tr key={transaction.id}>
+                                    <td width="40%">{transaction.description}</td>
+                                    <td>
+                                        <PriceHighlight variant={transaction.type}>
+                                           {transaction.type === 'outcome' && '- '}
+                                           {priceFormatter.format(transaction.price)}
+                                        </PriceHighlight>
+                                    </td>
+                                    <td>{transaction.category}</td>
+                                    <td>{dateFormatter.format(new Date(transaction.createdAt))}</td>
+                                </tr>
+                            )
+                        })}
                     </tbody>
                 </TransactionTable>
             </TransactionsContainer>
